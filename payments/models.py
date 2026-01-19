21cab10be853cf83e63c5b7f23ab746930ac7a9e
Evg Fomin <efominykhgh@gmail.com>
2026-01-20 00:22:27 +0300

    Add Post and Subscription models

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,33 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from users.models import User
+from posts.models import Post
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Subscription(models.Model):
+    PERIOD_CHOICES = [
+        (1, _("1 month")),
+        (3, _("3 months")),
+        (6, _("6 months")),
+        (9, _("9 months")),
+        (12, _("12 months")),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subscriptions")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subscribers", limit_choices_to={"is_author": True})
+    period_months = models.PositiveSmallIntegerField(_("period in months"), choices=PERIOD_CHOICES)
+    price = models.DecimalField(_("price"), max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    start_date = models.DateTimeField(_("start date"), auto_now_add=True)
+    end_date = models.DateTimeField(_("end date"))
+    is_active = models.BooleanField(_("is active"), default=True)
+    stripe_subscription_id = models.CharField(_("Stripe subscription ID"), max_length=100, blank=True, null=True)
+    auto_renew = models.BooleanField(_("auto renew"), default=True)
+
+    class Meta:
+        verbose_name = _("subscription")
+        verbose_name_plural = _("subscriptions")
+        unique_together = ["user", "author"]
+
+    def __str__(self):
+        return f"{self.user.phone_number} subscribed to {self.author.phone_number} for {self.period_months} months"
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,34 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from users.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = _("tag")
+        verbose_name_plural = _("tags")
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(_("title"), max_length=200)
+    content = models.TextField(_("content"))
+    is_paid = models.BooleanField(_("is paid"), default=False)
+    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
+    youtube_link = models.URLField(_("YouTube link"), blank=True, null=True)
+    views = models.PositiveIntegerField(_("views"), default=0)
+    likes = models.PositiveIntegerField(_("likes"), default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("post")
+        verbose_name_plural = _("posts")
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.title} by {self.author.phone_number}"
