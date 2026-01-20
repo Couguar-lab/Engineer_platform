30235638a0a3892f4d373703bc90e5759e49dd3c
Evg Fomin <efominykhgh@gmail.com>
2026-01-21 00:32:27 +0300

    Fix NoReverseMatch: add post_list route and view + update navbar link

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from posts.views import post_detail
+from posts.views import post_detail, post_list
 
 urlpatterns = [
     path('<int:post_id>/', post_detail, name='post_detail'),
-    path('posts/', include('posts.urls')),
+    path('', post_list, name='post_list'),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,10 +4,14 @@ from django.shortcuts import render, get_object_or_404
 from posts.models import Post
 
 
-@login_required
 def post_detail(request, post_id):
-    """
-    Детальная страница поста с проверкой доступа.
-    """
     post = get_object_or_404(Post, id=post_id)
-    return render(request, 'posts/post_detail.html', {'post': post})
+    has_access = post.can_view(request.user)
+    return render(request, 'posts/post_detail.html', {
+        'post': post,
+        'has_access': has_access
+    })
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', {'posts': posts})
