eb90c2c5a1cb69ee2936dde4f686dd547bd29e00
Evg Fomin <efominykhgh@gmail.com>
2026-01-20 20:24:13 +0300

    Register admin classes for Post, Tag, Subscription, Notification, Theme

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,19 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    """
+    Админка для уведомлений пользователей.
+    """
+    list_display = ('user', 'message_short', 'is_read', 'created_at')
+    list_filter = ('is_read',)
+    search_fields = ('user__phone_number', 'message')
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at',)
+    list_select_related = ('user',)
+
+    def message_short(self, obj):
+        return obj.message[:50] + "..." if len(obj.message) > 50 else obj.message
+    message_short.short_description = "Сообщение"
--- themes/admin.py
+++ themes/admin.py
@@ -1,3 +1,12 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Theme
 
-# Register your models here.
+
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    """
+    Админка для настроек тем пользователей.
+    """
+    list_display = ('user', 'theme')
+    search_fields = ('user__phone_number',)
+    list_select_related = ('user',)
