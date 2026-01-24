0f6402858d570ad48074e9ca0d025b8dbcd47ecb
Evg Fomin <efominykhgh@gmail.com>
2026-01-24 12:27:47 +0300

    fix settings

--- engineer_platform/settings.py
+++ engineer_platform/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=True)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1", ".ngrok-free.dev"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1", ".ngrok-free.dev", "89.169.191.228"])
 
 INSTALLED_APPS = [
     "django.contrib.admin",
